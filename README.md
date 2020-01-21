e8704c36107081fa5da062f15b9f1480f065403d
Pete Farquhar <petefarq@gmail.com>
2020-01-21 08:18:49 -0500

    v1 files 1-12-20

--- README.md
+++
@@ -1,1 +0,0 @@
-# cisnc-mapping
--- static/js/logic.js
+++ static/js/logic.js
@@ -2,86 +2,132 @@
 var myMap = L.map("map", {
     center: [35.2, -79.79],
     zoom: 8
-  });
+});
   
-  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "mapbox.light",
+    id: "mapbox.streets-basic",
     accessToken: API_KEY
-  }).addTo(myMap);
-  
+}).addTo(myMap);
+
 // Add county boundaries
 d3.json("NC_Counties.geojson", function(data) {
-  L.geoJson(data).addTo(myMap);
-});
+ 
+  county_colors = d3.csv("county_tier_colors.csv");
 
-// Add school points
-d3.json("df_schools_geojson.geojson", function(data) {
-  L.geoJson(data).addTo(myMap);
-});
+  function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    layer.bindTooltip(feature.properties.CO_NAME,
+    {className: 'popup', permanent: true, direction:"center", opacity: 1})
+  };
 
-// function createFeatures(earthquakeData) {
-
-//   // Define a function we want to run once for each feature in the features array
-//   // Give each feature a popup describing the place and time of the earthquake
-//   function onEachFeature(feature) {
-
-//         // Conditionals for circle color based on magnitude
-//         var color = "";
-//         if (feature.properties.mag > 3) {
-//             color = "#a50f15",
-//             fillcolor = "#a50f15"
-//         }
-//         else if (feature.properties.mag > 2.5) {
-//             color = "#de2d26",
-//             fillcolor = "#de2d26"
-//         }
-//         else if (feature.properties.mag > 2) {
-//             color = "#fb6a4a",
-//             fillcolor = "#fb6a4a"
-//         }
-//         else if (feature.properties.mag > 1.5) {
-//             color = "#fcae91",
-//             fillcolor = "#fcae91"
-//         }
-//         else {
-//             color = "#fee5d9",
-//             fillcolor = "#fee5d9"
-//         }
+  function style(feature) {
       
-//         // Add circles to map with popup
-//         L.circle([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
-//             fillOpacity: 0.4,
-//             color: color,
-//             fillColor: fillcolor,
-//             // Adjust radius
-//             radius: feature.properties.mag * 2500
-//             }).bindPopup("<h3>" + feature.properties.place + "<p>" + "Magnitude: "  + feature.properties.mag +
-//                 "</h3><hr><p>" + new Date(feature.properties.time) + "</p>").addTo(myMap);
+      // function getColor(d) {
 
-//   };
+      //     var color = "";
 
-//   L.geoJSON(earthquakeData, {
-//     onEachFeature: onEachFeature
-//   });
-
-// };
-
-// var legend = L.control({ position: "bottomleft" });
-
-// legend.onAdd = function() {
-//   var div = L.DomUtil.create("div", "legend");
-//   div.innerHTML += "<h3>Earthquakes Past 7 Days:<br>by Richter Scale</h3>";
-//   div.innerHTML += '<i style="background: #a50f15"></i><span> > 3</span><br>';
-//   div.innerHTML += '<i style="background: #de2d26"></i><span> > 2.5</span><br>';
-//   div.innerHTML += '<i style="background: #fb6a4a"></i><span> > 2</span><br>';
-//   div.innerHTML += '<i style="background: #fcae91"></i><span>> 1.5</span><br>';
-//   div.innerHTML += '<i style="background: #fee5d9"></i><span> <= 1.5</span><br>';
-//   div.innerHTML += '<i class="icon" style="background-image: url(https://d30y9cdsu7xlg0.cloudfront.net/png/194515-200.png);background-repeat: no-repeat;"></i>';
+      //     for (var i = 0; i < county_colors.length; i++) {
+      //       console.log(county_colors[i].CO_NAME);
+      //       if (county_colors[i].CO_NAME == CO_NAME) {
+      //         color = county_colors[i].hue;
+      //         break;
+      //       }
+      //     }
+      //     return color;
+      // };    
+        
+      return {
+          fillColor: "white",
+          fillOpacity: 0,
+          weight: 1,
+          opacity: 1,
+          color: "black"
+      }
+  }
   
-//   return div;
-// };
+  L.geoJson(data, {onEachFeature: onEachFeature, style: style, interactive: false}).addTo(myMap).openTooltip();
 
-// legend.addTo(myMap);
+}); 
+
+// Add school circles
+
+var cis_df_color = "#1f78b4"
+var cis_abc_color = "#33a02c"
+var df_color = "#ff7f00"
+
+d3.json("all_schools_gj.geojson", function(data) {
+
+  createFeatures(data.features)
+});
+
+function createFeatures(schoolData) {
+
+  function onEachFeature(feature) {
+        // Conditionals for circle color based on CISNC schools        
+        
+        var color = "";
+        if (feature.properties.CISNC_school == "yes" && (feature.properties.S_P_grade == "D" || feature.properties.S_P_grade == "F")) {
+            color = cis_df_color,
+            fillcolor = cis_df_color
+        }
+        else if (feature.properties.CISNC_school == "yes" && (feature.properties.S_P_grade != "D" && feature.properties.S_P_grade != "F")) {
+            color = cis_abc_color,
+            fillcolor = cis_abc_color
+        }
+        else {
+          color = df_color,
+          fillcolor = df_color
+        }
+        
+        // Add circles to map with popup
+        L.circle([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
+            fillOpacity: .5,
+            color: color,
+            fillColor: fillcolor,
+            radius: 1500
+            }).bindPopup("<p><strong>" + feature.properties.name + "</strong><br>" +
+                        feature.properties.district + "<br>" +
+                        "CISNC school: "  + feature.properties.CISNC_school + "<br>" +
+                        "CISNC Affiliate: "  + feature.properties.affiliate + "<br>" +
+                        "SP Grade: "  + feature.properties.S_P_grade + "<br>" +
+                        "EOG: "  + feature.properties.EOG + "%" +  "<br>" +
+                        "Growth: " + feature.properties.growth_status + "<br>" +
+                        "Graduation Rate: " + feature.properties.grad_rate + "%" + "<br>" +
+                        "Econ Disadvantaged: "  + feature.properties.percent_EDS + "%" + "</p>")
+            .addTo(myMap);   
+  }
+  
+  L.geoJSON(schoolData, {
+    onEachFeature: onEachFeature
+  });
+
+};
+
+// Add legend
+
+var legend = L.control({ position: "bottomleft" });
+
+legend.onAdd = function() {
+  var div = L.DomUtil.create("div", "legend");
+  div.innerHTML += `<i style="background: ${cis_abc_color}"></i><span>CISNC Schools, Grade A/B/C</span><br>`;
+  div.innerHTML += `<i style="background: ${cis_df_color}"></i><span style="color:black; font-size: 20px;">CISNC Schools, Grade D/F</span><br>`;
+  div.innerHTML += `<i style="background: ${df_color}"></i><span style="color:black; font-size: 20px;">Non-CISNC Schools, Grade D/F</span><br>`;
+  
+  return div;
+};
+
+legend.addTo(myMap);
+
+// Add Title
+var title = L.control({ position: "topright" });
+
+title.onAdd = function() {
+  var div = L.DomUtil.create("div");
+  div.innerHTML += `<span style="color:black; font-size: 24px;"><strong>CISNC Schools and D/F Performance Grade Schools</strong></span>`;
+  return div;
+};
+
+title.addTo(myMap);
 
